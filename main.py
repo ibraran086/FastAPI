a406bfefdb878245da1c0f4f334f6b77fa6d4db4
Ibrar Munir <ibraran086@gmail.com>
2026-09-18 22:32:56 +0500

    done

--- main.py
+++ main.py
@@ -692,10 +692,26 @@ def get_users(user:users):
         "message":"data updated",
         "data":user
     }
-from fastapi import FastAPI,status
+from fastapi import FastAPI,status,HTTPException
 app=FastAPI()
 @app.post("/users",status_code=status.HTTP_201_CREATED)
 def get():
     return {
         "message":"data updated"
+    }
+@app.get("/users")
+def data():
+    return {
+        "message":"done"
+    }
+@app.get("/users,{user_id}")
+def get_user(user_id:int):
+    if user_id!=1:
+        raise HTTPException(
+            status_code=404,
+            detail="user not found"
+        )
+    return {
+        "id":1,
+        "name":"ibrar"
     }
