27e0b8f09c7c8a972e5490df545ca500cf4dd176
Ibrar Munir <ibraran086@gmail.com>
2026-09-24 07:35:29 +0500

    done

--- main.py
+++ main.py
@@ -888,3 +888,21 @@ def secure_data(user=Depends(token)):
         "message":"secure data accessed",
         "user":user
     }
+from fastapi import FastAPI,Request
+app=FastAPI()
+@app.middleware("http")
+async def middle_ware(request:Request,call_next):
+    print("request received")
+    response=await call_next(request)
+    print("reponse sent")
+    return response
+from fastapi import FastAPI,Request
+import time
+app=FastAPI()
+@app.middleware("http")
+async def log_middleware(request:Request,call_next):
+    start_time=time.time()
+    response=await call_next(request)
+    process_time=time.time()-start_time
+    print(f"Path:{request.url.path}| Time:{process_time}")
+    return response
